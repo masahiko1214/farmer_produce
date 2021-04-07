ca6dbcbb71dc580b99610881ecc1d080831f4fa1
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-04-07 14:21:50 +0900

    ルーティングファイルの追加
    - customersコントローラに対応するルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
 
-  devise_for :customers
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
   root 'stocks#home'
   resources :stocks
   get 'stocks/home'
